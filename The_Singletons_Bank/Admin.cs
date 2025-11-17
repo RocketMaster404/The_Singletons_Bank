2958462f0b64fafb6c80621f3d53118b7e5758f5
DanielNilsson-GIT <daniel.gronberg92@gmail.com>
2025-11-17 15:22:05 +0100

    PairProgramming
    Userclasses created.

---
+++ The_Singletons_Bank/Admin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Singletons_Bank
+{
+    internal class Admin:User
+    {
+        public bool IsAdmin = true;
+
+
+
+        public Admin(string username, string password) : base(username, password)
+        {
+
+        }
+    }
+}
---
+++ The_Singletons_Bank/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Singletons_Bank
+{
+    internal abstract class User
+    {
+        private string _username;//Behövs detta??????
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public int LoginAttempts { get; set; } = 0;
+
+        public bool UserIsBlocked { get; set; } = false;
+        protected User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+            
+        }
+
+    }
+}
