04582d500c6c9cef3cb0e01735373fd3ffe4d541
DanielNilsson-GIT <daniel.gronberg92@gmail.com>
2025-11-20 16:13:46 +0100

    added outline for loans class

--- The_Singletons_Bank/Account.cs
+++ The_Singletons_Bank/Account.cs
@@ -6,89 +6,92 @@ using System.Threading.Tasks;
 
 namespace The_Singletons_Bank
 {
-   internal class Account
-   {
-      private static List<int> usedAccountNumbers = new(); 
-      private int _accountNumber { get; set; }
-      private decimal _balance { get; set; }
-      private string _currency { get; set; } = "SEK";
+    public class Account
+    {
+        private static List<int> usedAccountNumbers = new();
+        private int _accountNumber { get; set; }
+        private decimal _balance { get; set; }
+        private string _currency { get; set; } = "SEK";
 
-      public Account(decimal balance, string currency)
-      {
-         _accountNumber = GenerateAccountNumber();
-         _balance = balance;
-         _currency = currency;
-          
-      }
+        public Account(decimal balance, string currency)
+        {
+            _accountNumber = GenerateAccountNumber();
+            _balance = balance;
+            _currency = currency;
 
+        }
 
-      public int GenerateAccountNumber()
-      {
-         Random random = new Random();
-         bool uniqueNumber;
-         int number;
-         do
-         {
-            uniqueNumber = true;
-            number = random.Next(10000000, 99999999);
-            foreach (var existingAccountNumber in usedAccountNumbers)
+        public decimal ShowAccBalance()
+        {
+            return _balance;
+        }
+        public int GenerateAccountNumber()
+        {
+            Random random = new Random();
+            bool uniqueNumber;
+            int number;
+            do
             {
-               if (number == existingAccountNumber)
-               {
-                  uniqueNumber = false;
-               }
+                uniqueNumber = true;
+                number = random.Next(10000000, 99999999);
+                foreach (var existingAccountNumber in usedAccountNumbers)
+                {
+                    if (number == existingAccountNumber)
+                    {
+                        uniqueNumber = false;
+                    }
+                }
+
+            } while (!uniqueNumber);
+
+            usedAccountNumbers.Add(number);
+            return number;
+        }
+
+        public void ShowAccount(Account account)
+        {
+            Console.WriteLine($"Kontoöversikt\n");
+            Console.WriteLine($"Kontonummer: {account._accountNumber}");
+            Console.WriteLine($"Saldo: {account._balance} {account._currency}");
+        }
+
+        public void CreateAccount(User user) // Behöver fixas så att man adderar till users lista
+        {
+            Console.WriteLine("Skapa konto");
+            Console.WriteLine("1. Konto i SEK");
+            Console.WriteLine("2. Konto i USD");
+            Console.WriteLine("3. Konto i EUR");
+            Console.Write("Ange valuta: ");
+            int input = Utilities.GetUserNumberMinMax(1, 3);
+
+            switch (input)
+            {
+                case 1:
+                    var accountSEK = new Account(1000, "SEK");
+                    Console.WriteLine("Konto skapat\n");
+                    ShowAccount(accountSEK);
+                    // Lägg till konto till users lista
+                    break;
+
+                case 2:
+                    var accountUSD = new Account(10, "USD");
+                    Console.WriteLine("Konto skapat\n");
+                    ShowAccount(accountUSD);
+                    // Lägg till konto till users lista
+                    break;
+
+                case 3:
+                    var accountEUR = new Account(10, "EUR");
+                    Console.WriteLine("Konto skapat\n");
+                    ShowAccount(accountEUR);
+                    // Lägg till konto till users lista
+                    break;
+
             }
 
-         } while (!uniqueNumber);
 
-         usedAccountNumbers.Add(number);
-         return number;
-      }
-
-      public void ShowAccount (Account account) 
-      {
-         Console.WriteLine($"Kontoöversikt\n");
-         Console.WriteLine($"Kontonummer: {account._accountNumber}");
-         Console.WriteLine($"Saldo: {account._balance} {account._currency}");
-      }
-
-      public  void CreateAccount(User user) // Behöver fixas så att man adderar till users lista
-      {
-         Console.WriteLine("Skapa konto");
-         Console.WriteLine("1. Konto i SEK");
-         Console.WriteLine("2. Konto i USD");
-         Console.WriteLine("3. Konto i EUR");
-         Console.Write("Ange valuta: ");
-         int input = Utilities.GetUserNumberMinMax(1, 3);
-
-         switch (input)
-         {
-            case 1:
-               var accountSEK = new Account(1000, "SEK");
-               Console.WriteLine("Konto skapat\n");
-               ShowAccount(accountSEK);
-               // Lägg till konto till users lista
-               break;
-               
-            case 2:
-               var accountUSD = new Account(10, "USD");
-               Console.WriteLine("Konto skapat\n");
-               ShowAccount(accountUSD);
-               // Lägg till konto till users lista
-               break;
-
-            case 3:
-               var accountEUR = new Account(10, "EUR");
-               Console.WriteLine("Konto skapat\n");
-               ShowAccount(accountEUR);
-               // Lägg till konto till users lista
-               break;
-
-         }
-
-         
-      }
+        }
 
 
-   }
+    }
 }
--- The_Singletons_Bank/Loans.cs
+++ The_Singletons_Bank/Loans.cs
@@ -8,35 +8,39 @@ namespace The_Singletons_Bank
 {
     internal class Loans
     {
-       internal static bool LoanLimit;
+        internal static bool LoanLimit;
 
-        
-        public static void TakeLoan(decimal loan)
-        {
-            LoanLimit=Loanrequest(loan);
+        internal double Interestrate;
 
-            if (LoanLimit = true)
-            {
-                Console.WriteLine($"Ditt lån har blivit beviljat.\nDu har lånat -{loan}Kr");
-                Account.Balance = Account.Balance + loan;
 
-            }
+        //public static void TakeLoan(Account user)
+        //{
+        //    decimal loanlimit=user.ShowAccBalance();
+        //    LoanLimit = Loanrequest(loanlimit);
 
-            else
-            {
-                Console.WriteLine($"Du kan inte låna mer än" + { Account.balance}*5+"\nDitt lån har blivit nekat");
-            }
-        }
+        //    if (LoanLimit = true)
+        //    {
+        //        Console.WriteLine($"Ditt lån har blivit beviljat.\nDu har lånat -{loan}Kr");
+        //         =  + loan;
 
-        public static bool Loanrequest(decimal loanRequest)
-        {
-            int balance = Account.balance;
-            if (loanRequest >= (balance * 5))
-            {
-                return false;
-            }
-            return true;
-        }
+        //    }
+
+        //    else
+        //    {
+        //        Console.WriteLine($"Du kan inte låna mer än" + { account.balance}
+        //        *5 + "\nDitt lån har blivit nekat");
+        //    }
+        //}
+
+        //public static bool Loanrequest(decimal loanRequest)
+        //{
+        //    decimal balance =;
+        //    if (loanRequest >= (balance * 5))
+        //    {
+        //        return false;
+        //    }
+        //    return true;
+        //}
 
     }
 }
