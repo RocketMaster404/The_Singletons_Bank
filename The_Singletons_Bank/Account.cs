dbdb2b290407560497864e5e2c925993e00274be
DanielNilsson-GIT <daniel.gronberg92@gmail.com>
2025-11-20 16:25:15 +0100

    Revert "Creating loan class"

--- The_Singletons_Bank/Account.cs
+++ The_Singletons_Bank/Account.cs
@@ -44,61 +44,10 @@ namespace The_Singletons_Bank
             number = random.Next(10000000, 99999999);
             foreach (var existingAccountNumber in usedAccountNumbers)
             {
-                uniqueNumber = true;
-                number = random.Next(10000000, 99999999);
-                foreach (var existingAccountNumber in usedAccountNumbers)
-                {
-                    if (number == existingAccountNumber)
-                    {
-                        uniqueNumber = false;
-                    }
-                }
-
-            } while (!uniqueNumber);
-
-            usedAccountNumbers.Add(number);
-            return number;
-        }
-
-        public void ShowAccount(Account account)
-        {
-            Console.WriteLine($"Kontoöversikt\n");
-            Console.WriteLine($"Kontonummer: {account._accountNumber}");
-            Console.WriteLine($"Saldo: {account._balance} {account._currency}");
-        }
-
-        public void CreateAccount(User user) // Behöver fixas så att man adderar till users lista
-        {
-            Console.WriteLine("Skapa konto");
-            Console.WriteLine("1. Konto i SEK");
-            Console.WriteLine("2. Konto i USD");
-            Console.WriteLine("3. Konto i EUR");
-            Console.Write("Ange valuta: ");
-            int input = Utilities.GetUserNumberMinMax(1, 3);
-
-            switch (input)
-            {
-                case 1:
-                    var accountSEK = new Account(1000, "SEK");
-                    Console.WriteLine("Konto skapat\n");
-                    ShowAccount(accountSEK);
-                    // Lägg till konto till users lista
-                    break;
-
-                case 2:
-                    var accountUSD = new Account(10, "USD");
-                    Console.WriteLine("Konto skapat\n");
-                    ShowAccount(accountUSD);
-                    // Lägg till konto till users lista
-                    break;
-
-                case 3:
-                    var accountEUR = new Account(10, "EUR");
-                    Console.WriteLine("Konto skapat\n");
-                    ShowAccount(accountEUR);
-                    // Lägg till konto till users lista
-                    break;
-
+               if (number == existingAccountNumber)
+               {
+                  uniqueNumber = false;
+               }
             }
 
          } while (!uniqueNumber);
