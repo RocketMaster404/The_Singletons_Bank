ba44ffee34069d216de6614407f339b50163836f
erik ny <erik.nyy@gmail.com>
2025-12-01 10:52:22 +0100

    Changed Customer and Admin menuchoice to a bool. Fixed the problem with users returning to login screen instead of mainmenu

--- The_Singletons_Bank/RunProgram.cs
+++ The_Singletons_Bank/RunProgram.cs
@@ -6,28 +6,28 @@ using System.Threading.Tasks;
 
 namespace The_Singletons_Bank
 {
-    internal class RunProgram
-    {
-        private static bool runOnce = true;
-      
+   internal class RunProgram
+   {
+      private static bool runOnce = true;
+
 
       public static void Run()
       {
          bool runProgram = true;
 
+
          while (runProgram)
          {
             User? user = null;
 
-            
-            if (runOnce)
-            {
-               Utilities.AsciiArtPrinter(runOnce);
-               runOnce = false;
-            }
 
-            
-            
+            Console.Clear();
+            Utilities.AsciiArtPrinter(runOnce);
+
+
+
+
+
             Menu.PrintLogInMenu();
             int input = Utilities.GetUserNumberMinMax(1, 2);
 
@@ -61,15 +61,25 @@ namespace The_Singletons_Bank
 
       public static void RunCustomerProgram(Customer customer)
       {
-         Console.WriteLine($"Inloggad användare {customer.GetUsername()}");
-         Menu.PrintCustomerMainMenu();
-         Menu.CustomerMainMenuChoice(customer);
+         bool loggedIn = true;
+         while (loggedIn)
+         {
+            Console.WriteLine($"Inloggad användare {customer.GetUsername()}");
+            Menu.PrintCustomerMainMenu();
+            loggedIn = Menu.CustomerMainMenuChoice(customer);
+         }
+
       }
 
       public static void RunAdminProgram(Admin admin)
       {
-         Menu.PrintAdminMainMenu();
-         Menu.AdminMainMenuChoice(admin);
+         bool loggedIn = true;
+         while (loggedIn)
+         {
+            Menu.PrintAdminMainMenu();
+            loggedIn = Menu.AdminMainMenuChoice(admin);
+
+         }
       }
-    }
+   }
 }
